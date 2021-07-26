183381a6b6b00beeb24992b2864e31147a040ac8
Drew-E-Wilson <dwilsrock21@gmail.com>
2021-07-26 16:58:14 -0400

    fetched data and got it to display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
+
+  const [joke, setJoke] = useState({ body: [''] });
+
+  const getJoke = async () => {
+    try {
+      const res = await fetch("https://dad-jokes.p.rapidapi.com/random/joke", {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-key": `${process.env.REACT_APP_API_KEY}`,
+          "x-rapidapi-host": "dad-jokes.p.rapidapi.com"
+        }
+      })
+      const data = await res.json();
+      setJoke(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  console.log(JSON.stringify(joke, null, 4));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dad Joke Generator</h1>
+      <button onClick={getJoke}>Tell Me A Joke!</button>
+      <h2>SetUp</h2>
+      <h3>{joke.body[0].setup}</h3>
+      <h2>Punch Line</h2>
+      <h3>{joke.body[0].punchline}</h3>
     </div>
   );
 }
